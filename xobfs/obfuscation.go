0dc0e9b22d475517b17e22857feb4712ccf61b17
striter-no <275512857+agnostic-t@users.noreply.github.com>
2026-05-30 12:01:45 +0300

    [upd] basic fix for framing

--- xobfs/obfuscation.go
+++ xobfs/obfuscation.go
@@ -1,6 +1,8 @@
 package xobfs
 
 import (
+	"encoding/binary"
+	"io"
 	"net"
 
 	"github.com/agnostic-t/neutrino-core/obfuscation"
@@ -12,33 +14,68 @@ var _ obfuscation.Obfuscator = (*Obfuscator)(nil)
 type XOBFSConn struct {
 	net.Conn
 	Psk []byte
+
+	readBuf []byte
 }
 
 func (c *XOBFSConn) Write(b []byte) (int, error) {
-	obfsData, err := algo.Obfuscate(b, c.Psk)
-	if err != nil {
-		return 0, err
+	maxChunk := 32768
+	totalWritten := 0
+
+	for len(b) > 0 {
+		chunkSize := min(len(b), maxChunk)
+
+		chunk := b[:chunkSize]
+		b = b[chunkSize:]
+
+		obfsData, err := algo.Obfuscate(chunk, c.Psk)
+		if err != nil {
+			return totalWritten, err
+		}
+
+		frame := make([]byte, 2+len(obfsData))
+		binary.BigEndian.PutUint16(frame[:2], uint16(len(obfsData)))
+		copy(frame[2:], obfsData)
+
+		if _, err := c.Conn.Write(frame); err != nil {
+			return totalWritten, err
+		}
+
+		totalWritten += chunkSize
 	}
 
-	_, err = c.Conn.Write(obfsData)
-	return len(b), err
+	return totalWritten, nil
 }
 
 func (c *XOBFSConn) Read(b []byte) (int, error) {
-	tempBuf := make([]byte, 8192)
-
-	n, err := c.Conn.Read(tempBuf)
-	if err != nil {
-		return n, err
+	if len(c.readBuf) > 0 {
+		n := copy(b, c.readBuf)
+		c.readBuf = c.readBuf[n:]
+		return n, nil
 	}
 
-	deobfData, err := algo.Deobfuscate(tempBuf[:n], c.Psk)
+	lenBuf := make([]byte, 2)
+	if _, err := io.ReadFull(c.Conn, lenBuf); err != nil {
+		return 0, err
+	}
+	frameLen := binary.BigEndian.Uint16(lenBuf)
+
+	frameBuf := make([]byte, frameLen)
+	if _, err := io.ReadFull(c.Conn, frameBuf); err != nil {
+		return 0, err
+	}
+
+	deobfData, err := algo.Deobfuscate(frameBuf, c.Psk)
 	if err != nil {
 		return 0, err
 	}
 
-	copy(b, deobfData)
-	return len(deobfData), nil
+	n := copy(b, deobfData)
+	if n < len(deobfData) {
+		c.readBuf = deobfData[n:]
+	}
+
+	return n, nil
 }
 
 type Obfuscator struct {
@@ -46,19 +83,9 @@ type Obfuscator struct {
 }
 
 func (o *Obfuscator) WrapConnTo(conn net.Conn) (net.Conn, error) {
-	wrapped := &XOBFSConn{
-		Conn: conn,
-		Psk:  o.Psk,
-	}
-
-	return wrapped, nil
+	return &XOBFSConn{Conn: conn, Psk: o.Psk}, nil
 }
 
 func (o *Obfuscator) WrapConnFrom(conn net.Conn) (net.Conn, error) {
-	wrapped := &XOBFSConn{
-		Conn: conn,
-		Psk:  o.Psk,
-	}
-
-	return wrapped, nil
+	return &XOBFSConn{Conn: conn, Psk: o.Psk}, nil
 }
